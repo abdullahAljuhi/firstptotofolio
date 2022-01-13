28e8f75e251a2adad74e7f522e5b7ccf0d5400dc
abdallah aljuhi <abdallahalju7@gmail.com>
2022-01-13 21:51:37 +0300

    edite js validation

--- js/validatin.js
+++ js/validatin.js
@@ -1,50 +1,38 @@
 let inputs= document.querySelectorAll('.input');
 let textarea= document.querySelector('textarea');
 let ok= document.querySelector('.submet');
-let alrt;
-let elm;
 inputs.forEach(element => {
-    elm=0
-    element.addEventListener('blur',(e)=>{
-        if(element.value.length < 1 || element.value==''){
-                alrt=e.target.parentElement.nextElementSibling;
-                elm=1;
-            if(alrt && elm){
-                if (alrt.classList.contains('valid')) {
-                    alrt.classList.remove('valid')
+    element.addEventListener('keyup',(e)=>{
+        if(element.value.length < 1 || element.value ==''){
+                if (e.target.parentElement.nextElementSibling.classList.contains('valid')) {
+                    e.target.parentElement.nextElementSibling.classList.remove('valid')
+                    e.target.parentElement.nextElementSibling.innerText="thia falid is reqired";
                 }
-                alrt.classList.add('novalid');
-            }
+                e.target.parentElement.nextElementSibling.classList.add('novalid');
         }
-            else if(elm){
-                if (alrt.classList.contains('novalid')) {
-                    alrt.classList.remove('novalid')
+            else {
+                if (e.target.parentElement.nextElementSibling.classList.contains('novalid')) {
+                    e.target.parentElement.nextElementSibling.classList.remove('novalid')
             }
-            alrt.classList.add('valid')
+            e.target.parentElement.nextElementSibling.classList.add('valid')
         }
-            
-    
     });
-    ok.disabled=false;
 });
 
 ok.addEventListener('click',function(e){
     e.preventDefault();
     inputs.forEach(element => {
-        if(element.value="" || element.value.length<1){
-            alrt=element.parentElement.nextElementSibling;
-        if(alrt){
-            if (alrt.classList.contains('valid')) {
-                alrt.classList.remove('valid')
+        if(element.value=="" || element.value.length < 1){
+            if (element.parentElement.nextElementSibling.classList.contains('valid')) {
+                element.parentElement.nextElementSibling.classList.remove('valid')
             }
-            alrt.classList.add('novalid');
-        }
+            element.parentElement.nextElementSibling.classList.add('novalid');
     }
-        else if(elm){
-            if (alrt.classList.contains('novalid')) {
-                alrt.classList.remove('novalid')
+        else {
+            if (element.parentElement.nextElementSibling.classList.contains('novalid')) {
+                element.parentElement.nextElementSibling.classList.remove('novalid')
         }
-        alrt.classList.add('valid')
+        element.parentElement.nextElementSibling.classList.add('valid')
     }
     })
 });
