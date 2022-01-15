c0bf5a07188db098f4752ef116b0644e353ecfc4
abdallah aljuhi <abdallahalju7@gmail.com>
2022-01-16 00:52:14 +0300

    add about and add some edit

--- js/app.js
+++ js/app.js
@@ -28,3 +28,24 @@ setInterval(function(){
     n=Math.floor(Math.random()*3);
     bq.style.backgroundImage = "url('"+imgs[n]+"')";
 },5000);
+
+
+let phone="738414111";
+let phoneIcon=document.querySelector(".icon-phone .fa-phone");
+let parphone=document.querySelector(".icon-phone p");
+let emil="abdallahalju7@gmil.com";
+let emailIcon=document.querySelector(".icon-email .fa-at");
+let paremail=document.querySelector(".icon-email p");
+hoverd(phoneIcon,phone,parphone);
+hoverd(emailIcon,emil,paremail);
+function hoverd(hoverd,content,element){
+    hoverd.addEventListener('mouseenter',(e)=>{
+        element.innerHTML = " ";
+        for (let i = 0; i < content.length; i++) {
+            element.innerHTML += content[i];
+        }
+    });
+    hoverd.addEventListener('mouseleave',()=>{
+        element.innerHTML = " ";
+    });
+}
